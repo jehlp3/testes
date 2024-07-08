68a6d13901a51f0faae9f038f979f5c5513c4363
jehlp3 <jessicalacerdap@hotmail.com>
2024-07-08 12:38:01 -0300

    create Controller

---
+++ todolist/src/main/java/br/com/todolist/controller/TodoController.java
@@ -0,0 +1,38 @@
+package br.com.todolist.controller;
+
+import br.com.todolist.entity.Todo;
+import br.com.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> read(){
+        return todoService.read();
+    }
+
+    @PutMapping
+    List<Todo> update(Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable Long id){
+        return todoService.delete(id);
+    }
+}
